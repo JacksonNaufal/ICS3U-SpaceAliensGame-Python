adc0d35f8db466854f645dfcd8f13c378545b398
Jackson-Naufal <80114615+JacksonNaufal@users.noreply.github.com>
2022-03-30 14:15:33 -0400

    hello_world

---
+++ code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by: Jackson Naufal
+# Created on: March 2022
+# This is a "Hello, World! program, tested for space aliens.
+
+import ugame
+import stage
+
+
+def game_scene():
+    # this function is going to pop up on the main game_scene
+    
+    print("\n\n\n")
+    print("Hello, World!")
+    print("Bye, World!")
+
+    # repeat forever, game loop
+    while True:
+        pass # just a placeholder for now
+
+if __name__ == "__main__":
+    game_scene()
