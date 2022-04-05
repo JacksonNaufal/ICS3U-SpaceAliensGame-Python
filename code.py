8435e288bacbc2584df818a2f0e0038dd8e6a875
Jackson Naufal <Jackson.naufal@mths.ca>
2022-04-05 18:21:16 +0000

    FixedLinterErrors

--- code.py
+++ code.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 
 # Created by: Jackson Naufal
@@ -16,13 +15,12 @@ def game_scene():
     # image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
-    
-    # sets the background to image 0 
+
+    # sets the background to image 0
     # and the size (10x8 titels of the size 16x16)
     background = stage.Grid(image_bank_background, 10, 8)
-    
-    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
     # create a stage
     # set frame rate to 60 fps
@@ -33,16 +31,16 @@ def game_scene():
     # most likely will only render background once per game scnece
     game.render_block()
 
-
     # repeat forever, game loop
     while True:
         # get user input
-        
+
         # update game logic
-        
+
         # redraw Sprites
         game.render_sprites([ship])
-        game.tick ()
+        game.tick()
+
 
 if __name__ == "__main__":
     game_scene()
