72b4150f838bf580d378c51e748ce8f8fa7dd30f
Jackson-Naufal <80114615+JacksonNaufal@users.noreply.github.com>
2022-04-07 13:54:27 -0400

    UpdatedCode

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 # Created by: Jackson Naufal
@@ -14,13 +15,14 @@ def game_scene():
 
     # image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
-    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
-
-    # sets the background to image 0
+    image_bank_sprites = stage.Bank.from_bmp16 ("space_aliens.bmp")
+    
+    # sets the bacgrkound to image 0 
     # and the size (10x8 titels of the size 16x16)
     background = stage.Grid(image_bank_background, 10, 8)
-
+    
     ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+
 
     # create a stage
     # set frame rate to 60 fps
@@ -31,16 +33,35 @@ def game_scene():
     # most likely will only render background once per game scnece
     game.render_block()
 
+
     # repeat forever, game loop
     while True:
         # get user input
-
+        keys = ugame.buttons.get_pressed()
+        
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("Start")
+        if keys & ugame.K_SELECT:
+            print("Select")
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x -1, ship.y)
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y -1)
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x, ship.y + 1)
+        
         # update game logic
-
+        
         # redraw Sprites
         game.render_sprites([ship])
-        game.tick()
-
+        game.tick ()
+    
 
 if __name__ == "__main__":
     game_scene()
