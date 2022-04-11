c91f009ba827e96223c62c7bff92404677cfadf5
Jackson Naufal <Jackson.naufal@mths.ca>
2022-04-11 18:03:29 +0000

    SPACECODE

---
+++ constants.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by: Jackson Naufal
+# Created on: March 2022
+# This is a space aliens game.
+
+
+# Pybadge screen size is 160x128 and the sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 16
+SCREEN_GRID_Y = 8
+SCREEN_SIZE = 16
+TOTAL_NUMBER_ALIENS = 5
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
+
+
+if __name__ == "__main__":
+    game_scene()
