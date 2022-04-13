ddb587263ad02fc1ecd14ba3ea556ce72963a194
Jackson-Naufal <80114615+JacksonNaufal@users.noreply.github.com>
2022-04-13 19:03:53 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -8,9 +8,16 @@
 # Pybadge screen size is 160x128 and the sprites are 16x16
 SCREEN_X = 160
 SCREEN_Y = 128
-SCREEN_GRID_X = 16
+SCREEN_GRID_X = 10
 SCREEN_GRID_Y = 8
 SCREEN_SIZE = 16
 TOTAL_NUMBER_ALIENS = 5
 FPS = 60
 SPRITE_MOVEMENT_SPEED = 1
+# Using for button state 
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released"
+}
