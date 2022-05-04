2e01cb8f1333c10e4771d8e3764a128895b1f16e
Jackson Naufal <Jackson.naufal@mths.ca>
2022-05-04 18:24:11 +0000

    Fixed Linter

--- constants.py
+++ constants.py
@@ -16,19 +16,21 @@ TOTAL_NUMBER_OF_LASERS = 5
 SHIP_SPEED = 1
 ALIEN_SPEED = 1
 LASER_SPEED = 2
-OFF_SCREEN_X = -100 
-OFF_SCREEN_Y = -100 
-OFF_TOP_SCREEN = -1 * SPRITE_SIZE 
+OFF_SCREEN_X = -100
+OFF_SCREEN_Y = -100
+OFF_TOP_SCREEN = -1 * SPRITE_SIZE
 OFF_BOTTOM_SCREEN = SCREEN_Y + SPRITE_SIZE
 FPS = 60
 SPRITE_MOVEMENT_SPEED = 1
-# Using for button state 
+# Using for button state
 button_state = {
     "button_up": "up",
     "button_just_pressed": "just pressed",
     "button_still_pressed": "still pressed",
-    "button_released": "released"
+    "button_released": "released",
 }
 # new pallet for filled text
-RED_PALETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-                b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
+RED_PALETTE = (
+    b"\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+    b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+)
